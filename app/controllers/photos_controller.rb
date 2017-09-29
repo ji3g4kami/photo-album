98eda2652f7cd9f08a8b8d8b6654e48a70710d33
Deng Li <ji3g4kami@gmail.com>
2017-09-29 23:55:31 +0800

    Basic CRUD done

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,43 @@
+class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    @photo.save
+
+    redirect_to photos_url
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+    @photo.update_attributes(photo_params)
+  
+    redirect_to photo_path(@photo)
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+
+    redirect_to root_path
+  end
+
+  private
+  def photo_params
+    params.require(:photo).permit(:title, :date, :descriprion, :file_location)
+  end
+end
